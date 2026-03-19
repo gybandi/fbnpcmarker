0876db36202eb6ee09b1f4a2e9a1830ad6feb4f0
Gyuro Andras <gyuro.andras@agilexpert.hu>
2026-03-19 21:49:46 +0100

    Added helper script, moved .gitignore

---
+++ package.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Files and directory to compress
+DIR_TO_ZIP="assets"
+FILES_TO_ZIP=("manifest.json" "content.js" "icon.png")
+OUTPUT_ZIP="build/fbnpcmarker.zip"
+
+# Create dir
+mkdir -p build
+# Create the zip archive
+zip -r "$OUTPUT_ZIP" "$DIR_TO_ZIP" "${FILES_TO_ZIP[@]}"
+
+echo "Created $OUTPUT_ZIP containing $DIR_TO_ZIP and ${FILES_TO_ZIP[*]}"
